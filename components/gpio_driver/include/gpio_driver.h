e222891aadc86d45c6a61899466d9369640a5d1e
Sief Ali <sief4136@gmail.com>
2026-05-08 16:58:03 +0300

    refactor: enhance gpio_driver API with improved function names and register handling
    Updated gpio_driver to improve clarity and consistency in function names, changing them to include the 'gpio_driver_' prefix. Refactored GPIO register access to utilize write-one-to-set and write-one-to-clear semantics for better performance. Added a toggle function and clarified the GPIO direction enumeration in the header file.

--- components/gpio_driver/include/gpio_driver.h
+++ components/gpio_driver/include/gpio_driver.h
@@ -5,14 +5,24 @@
 #include <stdint.h>
 
 typedef uint8_t gpio_pin_t;
-typedef volatile bool gpio_level_t;
+typedef bool gpio_level_t;
 
-#define GPIO_LEVEL_LOW false
-#define GPIO_LEVEL_HIGH true
+#define GPIO_LEVEL_LOW  ((gpio_level_t)false)
+#define GPIO_LEVEL_HIGH ((gpio_level_t)true)
 
+/* GPIO direction */
+typedef enum {
+    GPIO_DIRECTION_INPUT = 0U,
+    GPIO_DIRECTION_OUTPUT = 1U
+} gpio_direction_t;
+
+/* API */
 void gpio_driver_set_output(gpio_pin_t pin);
 void gpio_driver_set_input(gpio_pin_t pin);
+
 void gpio_driver_write(gpio_pin_t pin, gpio_level_t level);
+void gpio_driver_toggle(gpio_pin_t pin);
+
 gpio_level_t gpio_driver_read(gpio_pin_t pin);
 
 #endif
